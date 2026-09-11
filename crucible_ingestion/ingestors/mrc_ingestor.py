e0f5aa2caee0d67c5fcd775cc1b30c5e9b0d81bb
Peter Ercius <percius@lbl.gov>
2026-09-11 11:50:33 -0700

    ensure 3D data set or just get the first slice.

--- crucible_ingestion/ingestors/mrc_ingestor.py
+++ crucible_ingestion/ingestors/mrc_ingestor.py
@@ -99,8 +99,11 @@ class MrcIngestor(CrucibleDatasetIngestor):
         buf = None
         try:
             with nio.mrc.fileMRC(self.file_to_upload) as mrc1:
-                image_array = mrc1.getSlice(mrc1.dataSize[0] // 2)  # Get the middle slice for 3D data, or the only slice for 2D data
-            
+                if mrc1.dataSize.ndim ==3:
+                    image_array = mrc1.getSlice(mrc1.dataSize[0] // 2)  # Get the middle slice for 3D data, or the only slice for 2D data
+                else:
+                    image_array = mrc1.getSlice(0)  # Get the first slice for 2D data
+
             if image_array is None:
                 raise ValueError("No data found in MRC file.")
 
